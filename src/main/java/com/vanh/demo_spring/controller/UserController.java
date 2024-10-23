d3f620b8ed48854ef52f7339cd660bf20fba1add
vietanhyoyo <vanhyoyo@gmail.com>
2024-10-23 15:08:54 +0700

    update

--- src/main/java/com/vanh/demo_spring/controller/UserController.java
+++ src/main/java/com/vanh/demo_spring/controller/UserController.java
@@ -17,6 +17,8 @@ import com.vanh.demo_spring.dto.request.UserUpdateRequest;
 import com.vanh.demo_spring.entity.User;
 import com.vanh.demo_spring.service.UserService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -24,7 +26,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    User createUser(@RequestBody UserCreationRequest request) {
+    User createUser(@RequestBody @Valid UserCreationRequest request) {
         return userService.createUser(request);
     }
 
--- src/main/java/com/vanh/demo_spring/dto/request/UserCreationRequest.java
+++ src/main/java/com/vanh/demo_spring/dto/request/UserCreationRequest.java
@@ -2,9 +2,16 @@ package com.vanh.demo_spring.dto.request;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Size;
+
 public class UserCreationRequest {
+    
+    @Size(min = 3, message = "Username must be at least 3 characters")
     private String username;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
     private String password;
+
     private String firstName;
     private String lastName;
     private LocalDate dob;
@@ -12,30 +19,39 @@ public class UserCreationRequest {
     public String getUsername() {
         return username;
     }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
     public String getPassword() {
         return password;
     }
+
     public void setPassword(String password) {
         this.password = password;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public LocalDate getDob() {
         return dob;
     }
+
     public void setDob(LocalDate dob) {
         this.dob = dob;
     }
