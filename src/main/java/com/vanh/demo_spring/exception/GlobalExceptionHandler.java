741778bf84fab8ee112877d22538e51c061de5a3
vietanhyoyo <vanhyoyo@gmail.com>
2024-11-05 15:17:51 +0700

    update validator

--- src/main/java/com/vanh/demo_spring/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vanh/demo_spring/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.vanh.demo_spring.exception;
 
+import jakarta.validation.ConstraintViolation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,8 +9,13 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.vanh.demo_spring.dto.request.ApiResponse;
 
+import java.util.Map;
+import java.util.Objects;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final String MIN_ATTRIBUTE = "min";
 
     @SuppressWarnings("rawtypes")
     @ExceptionHandler(value = RuntimeException.class)
@@ -53,8 +59,13 @@ public class GlobalExceptionHandler {
 
         ErrorCode errorCode = ErrorCode.INVALID_KEY;
 
+        Map<String, Object> attributes = null;
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+
+            var constraintViolation = exception.getBindingResult().getAllErrors().get(0).unwrap(ConstraintViolation.class);
+            attributes = constraintViolation.getConstraintDescriptor().getAttributes();
+
         } catch (IllegalArgumentException e) {
 
         }
@@ -62,9 +73,17 @@ public class GlobalExceptionHandler {
         ApiResponse apiResponse = new ApiResponse();
 
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ?
+                mapAttribute(errorCode.getMessage(), attributes) :
+                errorCode.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes) {
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 
 }
