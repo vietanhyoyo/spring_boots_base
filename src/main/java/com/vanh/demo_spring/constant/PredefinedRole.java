5cbdccdf794622aeb4a94044d40d411838e72ea4
vietanhyoyo <vanhyoyo@gmail.com>
2024-11-15 14:35:03 +0700

    final unique username

---
+++ src/main/java/com/vanh/demo_spring/constant/PredefinedRole.java
@@ -0,0 +1,8 @@
+package com.vanh.demo_spring.constant;
+
+public class PredefinedRole {
+    public static final String USER_ROLE = "USER";
+    public static final String ADMIN_ROLE = "ADMIN";
+
+    private PredefinedRole() {}
+}
--- src/main/java/com/vanh/demo_spring/service/UserService.java
+++ src/main/java/com/vanh/demo_spring/service/UserService.java
@@ -3,6 +3,8 @@ package com.vanh.demo_spring.service;
 import java.util.HashSet;
 import java.util.List;
 
+import com.vanh.demo_spring.entity.Role;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,7 +15,7 @@ import com.vanh.demo_spring.dto.request.UserCreationRequest;
 import com.vanh.demo_spring.dto.request.UserUpdateRequest;
 import com.vanh.demo_spring.dto.response.UserResponse;
 import com.vanh.demo_spring.entity.User;
-import com.vanh.demo_spring.enums.Role;
+import com.vanh.demo_spring.constant.PredefinedRole;
 import com.vanh.demo_spring.exception.AppException;
 import com.vanh.demo_spring.exception.ErrorCode;
 import com.vanh.demo_spring.mapper.UserMapper;
@@ -37,19 +39,23 @@ public class UserService {
     private final RoleRepository roleRepository;
 
     public UserResponse createUser(UserCreationRequest request) {
-        log.info("Service: Create User");
-
-        if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
+        // if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
 
         User user = userMapper.toUser(request);
         user.setPassword(passwordEncoder.encode(request.getPassword()));
 
-        HashSet<String> roles = new HashSet<>();
-        roles.add(Role.USER.name());
+        HashSet<Role> roles = new HashSet<>();
+        roleRepository.findById(PredefinedRole.USER_ROLE).ifPresent(roles::add);
 
-        // user.setRoles(roles);
+        user.setRoles(roles);
 
-        return userMapper.toUserResponse(userRepository.save(user));
+        try {
+            user = userRepository.save(user);
+        } catch (DataIntegrityViolationException exception){
+            throw new AppException(ErrorCode.USER_EXISTED);
+        }
+
+        return userMapper.toUserResponse(user);
     }
 
     @PreAuthorize("hasRole('ADMIN')")
